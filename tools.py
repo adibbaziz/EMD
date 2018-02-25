ee2ff1b0d0456f70653c38b098923b96ddba5d04
adibbaziz <adib@Wheatley.local>
2018-02-25 17:51:56 +0100

    final

--- tools.py
+++ tools.py
@@ -10,11 +10,48 @@ def generate_normal(m,mu):
 	return P_points, P_weights
 
 
-def generate_mixture_gaussians(m):
-	
+def generate_mixture_gaussians(k):
+	X = []
+	for i in range(k):
+		x,y = np.random.randint(10), np.random.randint(10)
+		C,_ = generate_normal(20, [x,y])
+		X += list(C)
+	return np.array(X)
 
 
-def compare_clusterings(C1, C2):
+def compare_kmeans(C1, C2, unif):
+	"""
+	C1 and C2 are obtained from the attribute kmeans.labels_
+	"""
+	N = len(C1)
+	K, J = max(C1)+1, max(C2)+1
+
+	zeta = np.zeros((N,K))
+	gamma = np.zeros((N, J))
+	for i in range(N):
+		zeta[i, C1[i]] = 1
+		gamma[i, C2[i]] = 1
+	zeta = zeta.T
+	gamma = gamma.T
+	if unif:
+		alpha = [1./K for _ in range(K)]
+		beta = [1./J for _ in range(J)]
+	else:
+		alpha = [len(np.where(C1 == j)[0]) for j in range(K)]
+		beta = [len(np.where(C2 == j)[0]) for j in range(J)]
+
+	# compute EMD
+
 	d = lambda x,y: np.linalg.norm(x-y, ord=1)
+	emd = EMD(zip(zeta, alpha), zip(gamma, beta), d)
+	emd.write_lp_problem('emd_pb.lp')
+	emd.solve_lp()
+	return emd.distance
 
-	
+
+
+
+
+
+
+
